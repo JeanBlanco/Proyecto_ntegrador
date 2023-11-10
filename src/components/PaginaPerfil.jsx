ff2a84356db2256969aab07675861033792c9bef
DanielPaezR <daniel.paezr@unac.edu.co>
2023-11-09 21:10:24 -0500

    ultimo avance de perfil dropzone

--- src/components/PaginaPerfil.jsx
+++ src/components/PaginaPerfil.jsx
@@ -15,16 +15,43 @@ import ButtonCerrarSesion from "./ButtonCerrarSesion";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import "../styles/Perfil.css";
+import { useState } from "react";
+import Dropzone from "react-dropzone";
 
+  
 
 function PaginaPerfil() {
   const usuario = localStorage.getItem("username");
   const navigate = useNavigate();
   const MySwal = withReactContent(Swal);
   
-  const goToAjustes = () => {
-    navigate("/Ajustes");
-  };
+  
+  function UploadFiles() {
+    const [uploadedImage, setUploadedImage] = useState(null);
+  
+    const handleDrop = (acceptedFiles) => {
+      const image = acceptedFiles[0];
+      setUploadedImage(URL.createObjectURL(image));
+    };
+  
+    return (
+      <div>
+      <Dropzone onDrop={handleDrop}>
+        {({ getRootProps, getInputProps }) => (
+          <div {...getRootProps()} className="dropzone">
+            <input {...getInputProps()} />
+            <p>Arrastra y suelta una imagen aquí, o haz clic para seleccionar una.</p>
+          </div>
+        )}
+      </Dropzone>
+      {uploadedImage && (
+        <div>
+            <img src={uploadedImage} />
+        </div>
+      )}
+    </div>
+  );
+}
 
   const pregunta = () => {
     MySwal.fire({
@@ -55,15 +82,20 @@ function PaginaPerfil() {
         
     </div>
     <div className="cardGrande2">
-        <img className="ImagenPortada" src={ImagenPortada} alt="ImagenPortada"/>
-        <img className="ImagenPerfil" src={ImagenPerfil} alt="ImagenPerfil"/>
+        <div className="ImagenPortada">
+            <UploadFiles/>  
+        </div>
+        <div className="ImagenPerfil">
+            <UploadFiles/>
+        </div>
+        
         <label className="descripcion"> Horario de Atención</label>
         <label className="descripcion"> Lunes a Viernes: 7:00 AM -- 8:00 PM </label>
         <label className="descripcion"> Sábados, Domingos y Festivos: 9:00 AM -- 4:00 PM </label>
         <label className="direccion">Tv 39 #73b-95 a 73b-1, Laureles - Estadio, Medellín, Laureles, Medellín, Antioquia</label>
         <a className ="direccion" href = "https://www.google.com/maps/place/Caf%C3%A9+Zeppelin/@6.2463377,-75.600114,17z/data=!3m1!4b1!4m6!3m5!1s0x8e44299f7d809b6d:0x86ccf77109148212!8m2!3d6.2463324!4d-75.5975391!16s%2Fg%2F11ckqt10f3?hl=es&entry=ttu">Ver en Maps</a>
         <label className="slogan"> "Trabajamos por ti..." </label>
-        
+    
         <div className="cardImagenes">
             <scroll-container>
                 <img className="imagen"src={menu} alt="Imagen"/>
